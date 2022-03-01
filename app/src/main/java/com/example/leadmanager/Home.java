f75a8e0d9d286f195043dd7286e70d65a7b35a6e
parvathy-s <parvathysajeev0@gmail.com>
2022-03-01 12:50:01 +0530

    navigation bar

--- app/src/main/java/com/example/leadmanager/Home.java
+++ app/src/main/java/com/example/leadmanager/Home.java
@@ -1,25 +1,70 @@
 package com.example.leadmanager;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-public class Home extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        TextView usr = (TextView) findViewById(R.id.usr);
-        SessionManagement sessionManagement = new SessionManagement(Home.this);
-        String isLoggedIn = sessionManagement.getSession();
-        usr.setText(isLoggedIn);
+        Toolbar toolbar=findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        drawer= findViewById(R.id.drawer_layout);
+        NavigationView navigationView= findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.nav_open, R.string.nav_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+//        TextView usr = (TextView) findViewById(R.id.usr);
+//        SessionManagement sessionManagement = new SessionManagement(Home.this);
+//        String isLoggedIn = sessionManagement.getSession();
+//        usr.setText(isLoggedIn);
     }
 
-    public void onLogout(View view) {
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProfileFragment()).commit();
+                break;
+            case R.id.nav_logout:
+                onLogout();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+
+    }
+
+    private void onLogout() {
         SessionManagement sessionManagement= new SessionManagement(Home.this);
         sessionManagement.removeSession();
         openLogin();
